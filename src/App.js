c86e5475370fabc7baef9c239ec6ba3b04d12973
inafontan@gmail.com <you@example.com>
2023-07-28 20:38:11 +0200

    Contador de palabras ====================

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState('');
+    const wordCount = text.trim().split(/\s+/).length;
+
+    const handleChange = (event) => {
+        setText(event.target.value);
+    };
+
+    return (
+        <div className="App">
+            <h1>Word Counter App</h1>
+            <textarea placeholder="Escribe o pega tu texto aquí..."
+                value={text}
+                onChange={handleChange}/>
+            <p>Total de palabras: {wordCount}</p>
+        </div>
+    );
 }
 
 export default App;
